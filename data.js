39c2a0105be2a7e2f88eebf5bc23575e014175e4
M <maicmiller42@gmail.com>
2021-03-13 14:58:06 -0300

    Alimentando o player com os dados do audio.

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 window.audios = [
   {
-    title: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. ",
-    artist: "Lorem ipsum dolor sit",
-    cover: "Lorem-ipsum.jpg",
-    file: "Lorem-ipsum.mp3"
+    title: " Titulo Lorem ipsum dolor sit. ",
+    artist: "Lorem ipsum",
+    cover: "./Assets/Images/Untitled.jpg",
+    file: "./Assets/Audios/VivaLaVida.mp3"
   },
   {
     title: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. ",
--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 /**
  *
 */
+const cover = document.querySelector(".card-image");
+const title = document.querySelector(".card-content h5");
+const artist = document.querySelector(".artist");
+const audio = document.querySelector("audio");
+
+const data = {
+    title: " Titulo Lorem ipsum dolor sit. ",
+    artist: "Ouça nosso convidado de hoje!",
+    cover: "./Assets/Images/Untitled.jpg",
+    file: "./Assets/Audios/VivaLaVida.mp3"
+};
+
+cover.style.background = `url(${data.cover}) no-repeat center center / cover`;
+title.innerText = data.title;
+artist.innerHTML = `<i class='material-icons icon-image-preview'>hearing</i> ${data.artist}`;
+audio.src = data.file;
