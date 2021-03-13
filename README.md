44fab1e0ee75fc2bd5c887e6121de7089fc90bc0
M <maicmiller42@gmail.com>
2021-03-13 12:49:53 -0300

    Criação da estrutura base do projeto Audio Player PodCast

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Audio-Player
+
+Iniciando o projeto base que irá simular um player de podcast.
---
+++ data.js
@@ -0,0 +1,32 @@
+window.audios = [
+  {
+    title: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. ",
+    artist: "Lorem ipsum dolor sit",
+    cover: "Lorem-ipsum.jpg",
+    file: "Lorem-ipsum.mp3"
+  },
+  {
+    title: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. ",
+    artist: "Lorem ipsum dolor sit",
+    cover: "Lorem-ipsum.jpg",
+    file: "Lorem-ipsum.mp3"
+  },
+  {
+    title: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. ",
+    artist: "Lorem ipsum dolor sit",
+    cover: "Lorem-ipsum.jpg",
+    file: "Lorem-ipsum.mp3"
+  },
+  {
+    title: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. ",
+    artist: "Lorem ipsum dolor sit",
+    cover: "Lorem-ipsum.jpg",
+    file: "Lorem-ipsum.mp3"
+  },
+  {
+    title: " Lorem ipsum dolor sit, amet consectetur adipisicing elit. ",
+    artist: "Lorem ipsum dolor sit",
+    cover: "Lorem-ipsum.jpg",
+    file: "Lorem-ipsum.mp3"
+  }
+];
