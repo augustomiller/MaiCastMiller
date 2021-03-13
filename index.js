f0328f29bd3d6d4cde2ae2112b052aca6a10cf36
M <maicmiller42@gmail.com>
2021-03-13 16:36:44 -0300

    :pencil: encapsulando o player em um objeto.

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 /**
  *
 */
-const cover = document.querySelector(".card-image");
-const title = document.querySelector(".card-content h5");
-const artist = document.querySelector(".artist");
-const audio = document.querySelector("audio");
-
-const data = {
-    title: " Titulo Lorem ipsum dolor sit. ",
-    artist: "Lorem ipsum",
-    cover: "./Assets/Images/Untitled.jpg",
-    file: "./Assets/Audios/VivaLaVida.mp3"
+const player = {
+    cover: document.querySelector(".card-image"),
+    title: document.querySelector(".card-content h5"),
+    artist: document.querySelector(".artist"),
+    audio: document.querySelector("audio"),
+    data: {
+        title: " Titulo Lorem ipsum dolor sit. ",
+        artist: "Lorem ipsum",
+        cover: "./Assets/Images/Untitled.jpg",
+        file: "./Assets/Audios/VivaLaVida.mp3"
+    },
+    start() {
+        player.cover.style.background = `url('${player.data.cover}') no-repeat center center / cover`;
+        player.title.innerText = player.data.title;
+        player.artist.innerHTML = `<i class='material-icons icon-image-preview'>hearing</i> ${player.data.artist}`;
+        player.audio.src = player.data.file;
+    }
 };
 
-cover.style.background = `url('${data.cover}') no-repeat center center / cover`;
-title.innerText = data.title;
-artist.innerHTML = `<i class='material-icons icon-image-preview'>hearing</i> ${data.artist}`;
-audio.src = data.file;
+player.start();
