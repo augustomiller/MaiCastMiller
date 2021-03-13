95237ad2b45e2ddea3fea5e0cd17f818ac1be5a7
M <maicmiller42@gmail.com>
2021-03-13 19:37:41 -0300

    :hammer: refatorando o código.

--- data.js
+++ data.js
@@ -8,25 +8,25 @@ window.audios = [
   {
     title: "Paradise",
     artist: "Cold Play",
-    cover: "../Assets/Images/Untitled.jpg",
+    cover: "../Assets/Images/Untitled2.jpg",
     file: "../Assets/Audios/Paradise.mp3"
   },
   {
     title: "Smile",
     artist: "Cold Play",
-    cover: "../Assets/Images/Untitled.jpg",
+    cover: "../Assets/Images/Untitled3.jpg",
     file: "../Assets/Audios/Smile.mp3"
   },
   {
     title: "Brown",
     artist: "Cold Play",
-    cover: "../Assets/Images/Untitled.jpg",
+    cover: "../Assets/Images/Untitled4.jpg",
     file: "../Assets/Audios/Brown.mp3"
   },
   {
     title: "Up",
     artist: "Cold Play",
-    cover: "../Assets/Images/Untitled.jpg",
+    cover: "../Assets/Images/Untitled5.jpg",
     file: "../Assets/Audios/Up.mp3"
   }
 ];
--- player.js
+++ player.js
@@ -10,18 +10,24 @@ window.player = {
     currentAudio: {},
     currentPlaying: 0,
     start() {
-
+        this.update();
+        this.audio.onended = () => this.next();
+    },
+    next() {
+        this.currentPlaying++
+        if (this.currentPlaying == this.audioData.length) { this.restart(); } //Se o arquivo lido já é o ultimo, volta para o primeiro da fila.
+        this.update();
+        this.audio.play();//Se remover essa linha não funciona no firefox.
+    },
+    update() {
         this.currentAudio = this.audioData[this.currentPlaying];
-
         this.cover.style.background = `url('${path(this.currentAudio.cover)}') no-repeat center center / cover`;
         this.title.innerText = this.currentAudio.title;
         this.artist.innerHTML = `<i class='material-icons icon-image-preview'>hearing</i> ${path(this.currentAudio.artist)}`;
         this.audio.src = path(this.currentAudio.file);
-
-        this.audio.addEventListener("ended", () => {
-            this.currentPlaying++
-            this.audio.src = path(this.audioData[this.currentPlaying].file);
-            this.audio.play();
-        });
+    },
+    restart() {
+        this.currentPlaying = 0;
+        this.update;
     }
 };
