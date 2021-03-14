9233279ec1a768323b01e170fdc0d475badf30d3
M <maicmiller42@gmail.com>
2021-03-14 02:04:42 -0300

    :pencil: removendo o icone da tag artista.

--- player.js
+++ player.js
@@ -57,7 +57,7 @@ export default {
         this.currentAudio = this.audioData[this.currentPlaying];
         this.cover.style.background = `url('${path(this.currentAudio.cover)}') no-repeat center center / cover`;
         this.title.innerText = this.currentAudio.title;
-        this.artist.innerHTML = `<i class='material-icons icon-image-preview'>hearing</i> ${path(this.currentAudio.artist)}`;
+        this.artist.innerHTML = path(this.currentAudio.artist);
         elements.createAudioElement.call(this, path(this.currentAudio.file));
 
         this.audio.onloadeddata = () => { //Aguarda o metadata do audio ser carregado, para poder ser trabalhado.
